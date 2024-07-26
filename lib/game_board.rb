e7e19ddcf1933408b7848508a992f515c2350e25
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-07-25 20:13:56 -0400

    Add game_board.rb

---
+++ lib/game_board.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# stores state of the game board
+class GameBoard
+  def initialize
+  end
+
+  def print_board
+    # print board to console
+  end
+
+  def cell(row, col)
+    # get cell at position
+  end
+
+  def board
+    # get entire board
+  end
+
+  def update_cell(row, col)
+    # set cell to 
+  end
+end
