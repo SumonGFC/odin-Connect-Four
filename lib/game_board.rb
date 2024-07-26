c61ab7762699e004f1458c93f921c572c94cca55
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-07-26 03:41:09 -0400

    Refactor to accomodate new Column class

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,23 +1,38 @@
 # frozen_string_literal: true
 
+require_relative './column'
+
 # stores state of the game board
 class GameBoard
-  def initialize
+  attr_reader :board, :raw_board
+
+  def initialize(rows = 6, cols = 7)
+    @board = Array.new(cols) { Column.new(rows) }
+    @raw_board = @board.map(&:list)
   end
 
   def print_board
-    # print board to console
+    raw_board.transpose.each do |row|
+      puts row.map(&:value).join(' ')
+    end
+    puts("\n", (1..7).to_a.join(' '), "\n")
   end
 
-  def cell(row, col)
-    # get cell at position
-  end
-
-  def board
-    # get entire board
-  end
-
-  def update_cell(row, col)
-    # set cell to 
-  end
+  def update_cell(col, val); end
 end
+
+test = GameBoard.new
+test.print_board
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('O')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('A')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.board[0].update_next_cell('X')
+test.print_board
